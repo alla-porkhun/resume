b43f34457dbad9bda84c5d49cf56a27f2764ce25
alla.porkhun <all.forum17@gmail.com>
2018-11-21 16:13:59 +0200

    bank

--- js/main.js
+++ js/main.js
@@ -11,3 +11,4 @@ function loadNews(page){
     $(".content").load(page);
 }
 
+
---
+++ js/news.js
@@ -0,0 +1,17 @@
+$(function(){
+    let content = '';
+
+    let news = $.getJSON( "json/news.json", function() {
+        console.log( "success" );
+
+        let data = news.responseJSON.news;
+
+        for(let i = 0; i < data.length; i++) {
+            content += `<div class="title">${data[i].title}</div> <div class="title">${data[i].link}</div>`;
+        }
+
+        $(".content-news").html(content);
+    });
+
+
+});
